0eb42b2912f88cb282b394811fa54819b461cf71
chen <1164262698@qq.com>
2021-06-03 17:20:41 +0800

    端口修改

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/', router)
 
-const server = app.listen(5000,function(){
+const server = app.listen(5001,function(){
 	const { address, port } = server.address()
 	console.log('HTTP启动成功：http://%s:%s',address,port);
 })
