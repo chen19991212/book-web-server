f437ba62e4c927511ef2162269b986d85fbe75f9
chen <1164262698@qq.com>
2021-06-03 15:31:07 +0800

    修改uploadurl

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,9 @@
-const UPLOAD_PATH = 'F:/management-system/upload/upload-ebook'
+/* const UPLOAD_PATH = 'F:/management-system/upload/upload-ebook'
 const UPLOAD_URL = 'http://localhost:8001/upload-ebook'
-const OLD_UPLOAD_URL = 'http://localhost:8001/book'
+const OLD_UPLOAD_URL = 'http://localhost:8001/book' */
+
+const UPLOAD_PATH = '/usr/local/nginx/upload/upload-ebook'
+const UPLOAD_URL = 'http://121.41.178.240/upload-ebook'
 
 module.exports = {
   CODE_ERROR: -1,
